b26e088fea44e028b521c2912f5785fdf22c078c
Aldair Suarez <a219201031@unison.mx>
2023-11-14 18:31:09 -0700

    namespaces y exports e imports

---
+++ src/classes/Hero.ts
@@ -0,0 +1,25 @@
+import powers from "../data/powers"; //export default
+
+export class Hero {
+    constructor(
+        public name: string,
+        public powerId: number,
+        public age: number
+    ){}
+
+    get power():string { //return string
+        return powers.find( power => power.id === this.powerId)?.desc || "not found"
+    }
+}
+
+export class Hero1 {
+    
+}
+
+export class Hero2 {
+   
+}
+
+export const PI = 3.1416;
+
+export const miNombre = "Aldair Alexis"
